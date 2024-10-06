edbda6c2350c2090bf4fcb4c6b9625c1170b34d5
ElMatageis69 <juan.munoz.sanmartin000@gmail.com>
2024-10-06 20:43:09 -0300

    tailwind
    varias vainas medias lost

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './colaboracion.html',   // O donde esté tu archivo HTML
+    './colaboracion.js'  // O donde estén tus archivos JS
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
