9e9cee6bce5553c9dc14678a73e2b3e9dba000a0
ElMatageis69 <juan.munoz.sanmartin000@gmail.com>
2024-10-06 16:00:38 -0300

    2 calculadora

--- colaboracion.js
+++ colaboracion.js
@@ -1,3 +1,42 @@
 const input1 = document.getElementById("input1");
 const signo = document.getElementById("signo");
 const input2 = document.getElementById("input2");
+const resultado = document.getElementById("resultado");
+const btn = document.getElementById("btn");
+
+
+btn.addEventListener("click", function () {
+
+    let n1 = parseFloat(input1.value);
+    let operador = signo.value;
+    let n2 = parseFloat(input2.value);
+
+
+
+
+  if (
+    !isNaN(n1) &&
+    !isNaN(n2) &&
+    (operador === "+" ||
+      operador === "-" ||
+      operador === "/" ||
+      operador === "*")
+  ) {
+    switch (operador) {
+      case "+":
+        resultado.innerText = n1 + n2;
+        break;
+      case "-":
+        resultado.innerText = n1 - n2;
+        break;
+      case "*":
+        resultado.innerText = n1 * n2;
+        break;
+      case "/":
+        resultado.innerText = n1 / n2;
+        break;
+    }
+  } else {
+    resultado.innerText = "Pon algo valido";
+  }
+});
