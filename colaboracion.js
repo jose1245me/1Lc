50f75544b6f7546756f43fe5c222372b56311ea0
ElMatageis69 <juan.munoz.sanmartin000@gmail.com>
2024-10-20 19:58:03 -0300

    Avanzardo, incluí animaciones

--- colaboracion.js
+++ colaboracion.js
@@ -1,4 +1,25 @@
-const boton = document.getElementById("emailRegister");
-boton.addEventListener("click", ()=>{
+function animacion(oldDiv, newDiv) {
+    if (oldDiv.classList.contains("show")) {
+        oldDiv.classList.remove("show");
+    }
     
-})
+    oldDiv.classList.add("oculto");
+    
+    setTimeout(() => {
+        
+        
+        oldDiv.classList.add("Dnone");
+
+        newDiv.classList.remove("Dnone");
+        newDiv.classList.remove("oculto");
+        newDiv.classList.add("show");
+    }, 200);
+}
+
+document.getElementById("emailRegister").addEventListener("click", () => {
+    animacion(document.getElementById("preForm"), document.getElementById("register"));
+});
+
+document.getElementById("registerBack").addEventListener("click", () => {
+    animacion(document.getElementById("register"), document.getElementById("preForm"));
+});
